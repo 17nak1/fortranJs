180d4f3740279fc53320407bb344b967ea56301c
17nak1 <17na1@queensu.ca>
2019-09-11 14:11:06 -0400

    dasum

--- dasum.js
+++ dasum.js
@@ -1,63 +1,47 @@
 
-// c     takes the sum of the absolute values.
+/*
+*  @file         dasum.js
+*                dasum takes the sum of the absolute values.
+*  @inputs
+*   n            - number of elements in input vector.
+*
+*   dx           - double precision vector with n elements
+* 
+*   incx         - storage spacing between elements of dx
+*
+*  @author       Nazila Akhavan
+*  @date         Sep 2019
+*/
 
-// function dasum(n, dx, incx) {
-//   dasum = 0
-//   dtemp = 0
-//   if(n < 0) return 0
-//   if(incx === 1) {
-//     m = n - 6 * Math.floor(n / 6)
-//     if( m === 0 ) {
-//       mp1 = m + 1
-//       for(let i = mp1 - 1; i < n; i += 6) {
-//         dtemp = dtemp + Math.abs[dx[i]] + Math.abs[dx[i + 1]] + Math.abs[dx[i + 2]] + Math.abs[dx[i + 3]] + Math.abs[dx[i + 4]] + Math.abs[dx[i + 5]]
-//       }
-    
-//     for(let i = 1; i <= m; i++) {
-//       dtemp = dtemp + Math.abs[dx[i]]
-//     }
-//     if( n < 6 ) dasum = dtemp
-//   } else {
-//     ix = 1
-//     if(incx < 0) ix = (-n+1)*incx + 1
-//     for (i = 1; i <= n; i++) {
-//       dtemp = dtemp + Math.abs(dx(ix))
-//       ix = ix + incx
-//     }
-//     dasum = dtemp
-//   }
-//   return dasum
-// }
-
-function dasum(n, dx, incx) {
-  dasum = 0
+const dasum =  function (n, dx, incx) {
+  console.log(incx)
+  das = 0
   dtemp = 0
   if(n < 0) return 0
-  if(incx === 1) {
+  if(incx === 1) {//20
      m = n - 6 * Math.floor(n / 6)
-    if( m === 0 ) {
+    if( m === 0 ) {//40
       mp1 = m + 1
       for(let i = mp1 - 1; i < n; i += 6) {
-        dtemp = dtemp + Math.abs[dx[i]] + Math.abs[dx[i + 1]] + Math.abs[dx[i + 2]] + Math.abs[dx[i + 3]] + Math.abs[dx[i + 4]] + Math.abs[dx[i + 5]]
+        dtemp = dtemp + Math.abs(dx[i]) + Math.abs(dx[i + 1]) + Math.abs(dx[i + 2]) + Math.abs(dx[i + 3]) + Math.abs(dx[i + 4]) + Math.abs(dx[i + 5])
       }
+    } 
+    for(let i = 0; i < m; i++) {
+      dtemp = dtemp + Math.abs(dx[i])
     }
-      for(let i = 1; i <= m; i++) {
-      dtemp = dtemp + Math.abs[dx[i]]
-    }
-    if( n < 6 ) {
-      dasum = dtemp
-      return dasum
+    if( n < 6 ) {//60
+      das = dtemp
+      return das
     }
   }
-    ix = 1
-    if(incx < 0) ix = (-n+1)*incx + 1
-    for (i = 1; i <= n; i++) {
-      dtemp = dtemp + Math.abs(dx(ix))
-      ix = ix + incx
+    ix = 0
+    if(incx < 0) incx = -incx
+    for (i = 0; i < n; i++) {
+      if(dx[ix] || dx[ix] === 0) {
+        dtemp = dtemp + Math.abs(dx[ix])
+        ix = ix + incx
+      }
     }
-    dasum = dtemp
-      return dasum
+    das = dtemp
+      return das
 }
-
-
-console.log(dasum(2, [1,2], 1) )
