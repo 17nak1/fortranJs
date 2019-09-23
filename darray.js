3948260a12e0baa61d672c04f648da12a8be7cd2
kharazian <amirkharazian@gmail.com>
2019-09-23 12:08:12 +0330

    add change

--- darray.js
+++ darray.js
@@ -1,48 +1,50 @@
-let getfunc = function (target, prop, receiver) {
-    let j = 0,i = 0,index = 0            
-    if (typeof prop === 'symbol' || isNaN(+prop)){
-        if(!prop.includes(',')){                    
-            console.log(prop)
-            return target[prop]
+let xHandeler = {
+    get : function (target, prop, receiver) {
+            let j = 0,i = 0,index = 0            
+            if (typeof prop === 'symbol' || isNaN(+prop)){
+                if(!prop.includes(',')){                    
+                    console.log(prop)
+                    return target[prop]
+                }
+                else{
+                    temp =prop.split(',');
+                    i = parseInt(temp[0]);
+                    j = parseInt(temp[1]);
+                }                    
+            }
+            else{
+                i = parseInt(prop)
+            }
+            if(i > 0)
+                index = (j === 0) ? (target.base - 1) + i - 1 : (target.base - 1) + i - 1 + ((j - 1)*(target.u1 + 1 - 1));
+            return target.arr[index]    
+        },
+    set : function (target, prop, value, receiver) {
+            let j = 0,i = 0,index = 0            
+            if (typeof prop === 'symbol' || isNaN(+prop)){
+                if(prop.includes(',')){
+                    temp =prop.split(',');
+                    i = parseInt(temp[0]);
+                    j = parseInt(temp[1]);
+                }                    
+            }
+            else{
+                i = parseInt(prop)
+            }
+
+            if(i > 0)
+                index = (j === 0) ? (target.base - 1) + i - 1 : (target.base - 1) + i - 1 + ((j - 1)*(target.u1 + 1 - 1));
+            target.arr[index] = value;
         }
-        else{
-            temp =prop.split(',');
-            i = parseInt(temp[0]);
-            j = parseInt(temp[1]);
-        }                    
-    }
-    else{
-        i = parseInt(prop)
-    }
-    if(i > 0)
-        index = (j === 0) ? (target.base - 1) + i - 1 : (target.base - 1) + i - 1 + ((j - 1)*(target.u1 + 1 - 1));
-    return target.arr[index]    
-}
-let setFunc = function (target, prop, value, receiver) {
-    let j = 0,i = 0,index = 0            
-    if (typeof prop === 'symbol' || isNaN(+prop)){
-        if(prop.includes(',')){
-            temp =prop.split(',');
-            i = parseInt(temp[0]);
-            j = parseInt(temp[1]);
-        }                    
-    }
-    else{
-        i = parseInt(prop)
     }
 
-    if(i > 0)
-        index = (j === 0) ? (target.base - 1) + i - 1 : (target.base - 1) + i - 1 + ((j - 1)*(target.u1 + 1 - 1));
-    target.arr[index] = value;
-}
 let dArray  = function(base,u1,u2,arr){
     this.arr = arr === undefined ? [] : arr
     this.base = base === undefined ? 1 : base;  
     this.u1 = u1;
     this.u2 = u2;
 
-    return new Proxy(this, { get: getfunc, set: setFunc})
-  
+    return new Proxy(this, xHandeler)
 }
 
 dArray.prototype.clone = function(base,u1,u2) {
