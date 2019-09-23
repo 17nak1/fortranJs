d7e10f1e8eb8848baa4290b5ea957f02e39b3e0a
kharazian <amirkharazian@gmail.com>
2019-09-23 11:54:43 +0330

    solve with string

--- test copy.js
+++ test copy.js
@@ -1,7 +1,11 @@
 require('./dArray.js')
 
-a = [10,20,30,40,50].dArray(3)
+a = [10,20,30,40,50,60,70].dArray(1)
 
 b = a.clone(2)
+
+c = a.clone(1,2,3)
+
+d = a.clone(2,2,3)
 
 console.log('1111111111111')
